c77ae2680dd96cc7950e7b220b269402f3a67d1e
Donte Ravae <donteravae@gmail.com>
2020-09-07 02:48:58 -0400

    initial files for services menu scroller component created.

---
+++ src/components/services-menu-scroller/services-menu-scroller.component.jsx
@@ -0,0 +1,12 @@
+//React
+import React from 'react';
+//Styles
+import './services-menu-scroller.styles.scss';
+
+const ServicesMenuScroller = () => (
+    <section className="services-menu-scroller">
+        
+    </section>
+);
+
+export default ServicesMenuScroller;
