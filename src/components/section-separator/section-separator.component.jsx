eb4cebceb117e5935f9a2b3ab5c7330e7b491e62
Donte Ravae <donteravae@gmail.com>
2020-09-07 02:48:24 -0400

    section separator component completed. possible icons added at later date.

---
+++ src/components/section-separator/section-separator.component.jsx
@@ -0,0 +1,12 @@
+//React
+import React from 'react';
+//Styles
+import './section-separator.styles.scss';
+
+const SectionSeparator = () => (
+    <div className="section-separator">
+      <h1>A Cut Fit For A King</h1>
+    </div>
+);
+
+export default SectionSeparator;
