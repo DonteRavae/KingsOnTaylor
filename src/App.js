74bffd9c77e3b97c9209f630c80d1c041fcb9431
Donte Ravae <donteravae@gmail.com>
2020-09-06 23:45:28 -0400

    added landing and section separator components to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import React from 'react';
 //Components
 import HeaderNav from './components/header-nav/header-nav.component';
+import Landing from './components/landing/landing.component';
+import SectionSeparator from './components/section-separator/section-separator.component';
 //Styles
 import './App.css';
 
 const App = () => (
   <main className="App">
     <HeaderNav />
+    <Landing />
+    <SectionSeparator />
   </main>
 );
 
