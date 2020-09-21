a358b65a1b2e9eb95903aac27c76ec4e1970f2cb
Donte Ravae <donteravae@gmail.com>
2020-09-20 21:22:42 -0400

    service menu slider component created and styled

--- src/components/appointment-form/appointment-form.component.jsx
+++ src/components/appointment-form/appointment-form.component.jsx
@@ -1,73 +1,74 @@
 //React
-import React, {useState} from 'react';
+import React, { useState } from "react";
 //Components
-import FormInput, {SelectFormInput} from '../form-input/form-input.component';
+import FormInput, { SelectFormInput } from "../form-input/form-input.component";
+import ServicesMenuScroller from "../services-menu-scroller/services-menu-scroller.component";
 //Styles
-import './appointment-form.styles.scss';
+import "./appointment-form.styles.scss";
 
 const AppointmentForm = () => {
-    const [formValues, setFormValues] = useState({
-        firstName: '',
-        lastName: '',
-        phoneNumber: '',
-        barber: ''
-    })
+  const [formValues, setFormValues] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    barber: "",
+  });
 
-    const handleChange = event => {
-        const { name, value } = event.target;
-        setFormValues({...formValues, [name]: value})
-    }
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-    return (
-        <form className="appointment-form">
-            <header className="appointment-form-header">
-                <h3>NEED A CUT TODAY?</h3>
-                <h5>Book an appointment with us below!</h5>
-            </header>
-            <div className="name-inputs">
-                <FormInput
-                    name="firstName" 
-                    type="text"
-                    value={formValues.firstName}
-                    label="First Name"
-                    handleChange={handleChange}
-                    required
-                />
-                <FormInput
-                    name="lastName" 
-                    type="text"
-                    value={formValues.lastName}
-                    label="Last Name"
-                    handleChange={handleChange}
-                    required
-                />
-            </div>
-            <FormInput 
-                name="phoneNumber"
-                type="tel"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                value={formValues.phoneNumber}
-                label="Phone Number"
-                handleChange={handleChange}
-                format="Format: 803-555-5555"
-                required
-            />
-            <div className="barbers-and-times">
-                <SelectFormInput 
-                    name="barber"
-                    values={['Choose A Barber','tremaine', 'lee', 'dante']}
-                    required
-                />
-                <SelectFormInput 
-                    name="appointmentTimes"
-                    values={['Choose A Time']}
-                    required
-                />
-            </div>
+  return (
+    <form className="appointment-form">
+      <header className="appointment-form-header">
+        <h3>NEED A CUT TODAY?</h3>
+        <h5>Book an appointment with us below!</h5>
+      </header>
+      <div className="name-inputs">
+        <FormInput
+          name="firstName"
+          type="text"
+          value={formValues.firstName}
+          label="First Name"
+          handleChange={handleChange}
+          required
+        />
+        <FormInput
+          name="lastName"
+          type="text"
+          value={formValues.lastName}
+          label="Last Name"
+          handleChange={handleChange}
+          required
+        />
+      </div>
+      <FormInput
+        name="phoneNumber"
+        type="tel"
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        value={formValues.phoneNumber}
+        label="Phone Number"
+        handleChange={handleChange}
+        format="Format: 803-555-5555"
+        required
+      />
+      <div className="barbers-and-times">
+        <SelectFormInput
+          name="barber"
+          values={["Choose A Barber", "tremaine", "lee", "dante"]}
+          required
+        />
+        <SelectFormInput
+          name="appointmentTimes"
+          values={["Choose A Time"]}
+          required
+        />
+      </div>
 
-        </form>
-        
-    );
-}
+      <ServicesMenuScroller />
+    </form>
+  );
+};
 
 export default AppointmentForm;
--- src/components/services-menu-scroller/services-menu-scroller.component.jsx
+++ src/components/services-menu-scroller/services-menu-scroller.component.jsx
@@ -1,12 +1,46 @@
 //React
-import React from 'react';
+import React, { useState } from "react";
+//Components
+import ServiceItem from "../service-item/service-item.component";
+//Assets
+import HighFade from "../../assets/high-fade-haircut.jpeg";
+import TempFade from "../../assets/temple-fade-haircut.jpeg";
+import EvenCut from "../../assets/even-haircut.jpeg";
+import AfroTaper from "../../assets/afro-taper-haircut.jpeg";
 //Styles
-import './services-menu-scroller.styles.scss';
+import "./services-menu-scroller.styles.scss";
 
-const ServicesMenuScroller = () => (
+const ServicesMenuScroller = () => {
+  const [services, addServices] = useState([
+    {
+      style: "High Fade",
+      image: HighFade,
+    },
+    {
+      style: "Temple Fade",
+      image: TempFade,
+    },
+    {
+      style: "Even Cut",
+      image: EvenCut,
+    },
+    {
+      style: "Afro Taper",
+      image: AfroTaper,
+    },
+  ]);
+
+  return (
     <section className="services-menu-scroller">
-        
+      {services.map((service) => (
+        <ServiceItem
+          key={service.style}
+          style={service.style}
+          imgUrl={service.image}
+        />
+      ))}
     </section>
-);
+  );
+};
 
 export default ServicesMenuScroller;
