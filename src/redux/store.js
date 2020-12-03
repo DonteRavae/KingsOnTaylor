95a3580170372cf0003cf6d5668cc13720522bd5
Donte Ravae <donteravae@gmail.com>
2020-12-02 21:21:33 -0500

    redux store created

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,4 +3,6 @@ import { createStore } from "redux";
 //Reducers
 import { rootReducer } from "./rootReducer";
 
-export const store = createStore(rootReducer);
+const store = createStore(rootReducer);
+
+export default store;
