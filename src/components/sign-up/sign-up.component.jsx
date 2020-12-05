9129f29f8afd92c2cb04d71b40c4e9f0ed9904be
Donte Ravae <donteravae@gmail.com>
2020-12-05 03:27:18 -0500

    bcrypt added to project and SignUp component.

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -4,19 +4,72 @@ import { useState } from "react";
 //Components
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+//Async and Utilities
+import axios from "axios";
+import bcrypt from "bcryptjs";
 //Styles
 import "./sign-up.styles.scss";
 
 const INITIAL_FORM_VALUES = {
-  displayName: "",
+  firstName: "",
+  lastName: "",
   email: "",
+  phoneNumber: "",
   password: "",
   confirmPassword: "",
 };
 
+const createLoginWithEmailAndPassword = async (email, password) => {
+  //Hash password before checking
+  const hash = await bcrypt.hash(password, 12);
+  try {
+    let loginProfile = await axios.post(
+      "http://localhost:8080/loginProfileCheck",
+      {
+        email,
+        hash,
+      }
+    );
+
+    if (!loginProfile.data) {
+      alert("Email Address already exists!");
+    }
+    //If user doesn't exist in database return login profile. Data will return false if user exists.
+    return loginProfile.data;
+  } catch (error) {
+    console.log(error);
+  }
+  //Check if user exists in database with unique email address.
+};
+
+const createUserProfile = async (loginId, additionalInfo) => {
+  if (loginId === undefined) return;
+
+  try {
+    let user = await axios.post("http://localhost:8080/clientRegistration", {
+      loginId,
+      ...additionalInfo,
+    });
+
+    if (user.data) {
+      console.log(user.data);
+      return user.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const SignUp = () => {
   const [formValues, setFormValues] = useState(INITIAL_FORM_VALUES);
-  const { displayName, email, password, confirmPassword } = formValues;
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    password,
+    confirmPassword,
+  } = formValues;
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -32,19 +85,26 @@ const SignUp = () => {
       return;
     }
 
-    // try {
-    //   const { user } = await auth.createUserWithEmailAndPassword(email, password);
+    try {
+      const loginProfile = await createLoginWithEmailAndPassword(
+        email,
+        password
+      );
 
-    //   await createUserProfileDocument(user, { displayName });
-    //   setState({
-    //     displayName: '',
-    //     email: '',
-    //     password: '',
-    //     confirmPassword: ''
-    //   })
-    // } catch (error) {
-    //   console.error(error);
-    // }
+      if (loginProfile === undefined) {
+        return;
+      }
+
+      const client = await createUserProfile(loginProfile, {
+        firstName,
+        lastName,
+        phoneNumber,
+      });
+      console.log(client);
+      setFormValues(INITIAL_FORM_VALUES);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -54,10 +114,18 @@ const SignUp = () => {
       <form className="sign-up-form" onSubmit={handleSubmit}>
         <FormInput
           type="text"
-          name="displayName"
-          value={displayName}
+          name="firstName"
+          value={firstName}
           onChange={handleChange}
-          label="Display Name"
+          label="First Name"
+          required
+        />
+        <FormInput
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={handleChange}
+          label="Last Name"
           required
         />
         <FormInput
@@ -66,6 +134,16 @@ const SignUp = () => {
           value={email}
           onChange={handleChange}
           label="Email"
+          required
+        />
+        <FormInput
+          name="phoneNumber"
+          type="tel"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          value={phoneNumber}
+          label="Phone Number"
+          handleChange={handleChange}
+          format="Format: 803-555-5555"
           required
         />
         <FormInput
