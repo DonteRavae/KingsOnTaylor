e95a23c95b45abed01d6da94b775e663bb8cf8ed
Donte Ravae <donteravae@gmail.com>
2020-12-07 01:29:17 -0500

    Added new highlight section components.

--- src/pages/home-page/home.pages.jsx
+++ src/pages/home-page/home.pages.jsx
@@ -1,24 +1,22 @@
 //React
 import React from "react";
-//Pages
-// import BarbersPage from "../barbers-page/barbers.pages";
 //Components
-import HeaderNav from "../../components/header-nav/header-nav.component";
 import Landing from "../../components/landing/landing.component";
-import SectionSeparator from "../../components/section-separator/section-separator.component";
-// import BarbersOverview from "../../components/barbers-overview/barbers-overview.component";
+// import SectionSeparator from "../../components/section-separator/section-separator.component";
+import FamilyAtmosphere from "../../components/family-atmosphere/family-atmosphere.component";
+import DetailAndPrecision from "../../components/detail-and-precision/detail-and-precision.component";
+import MembersOverview from "../../components/members-overview/members-overview.component";
 //Styles
 import "./home.styles.scss";
 
 const HomePage = () => {
   return (
     <main className="App">
-      <HeaderNav />
       <Landing />
-      <SectionSeparator msg="Heavy Is The Head That Wears The Crown" />
-      {/* <BarbersOverview /> */}
-      <SectionSeparator msg="Style Section Separator.... Quote TBD" />
-      <SectionSeparator msg="Precision Section Separator.... Quote TBD" />
+      {/* <SectionSeparator msg="Heavy Is The Head That Wears The Crown" /> */}
+      <FamilyAtmosphere />
+      <DetailAndPrecision />
+      <MembersOverview />
     </main>
   );
 };
