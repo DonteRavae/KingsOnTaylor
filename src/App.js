639ef0f951dec913164e75a4457adc232b09afca
Donte Ravae <donteravae@gmail.com>
2020-12-14 15:40:57 -0500

    removed BarbersPage Component and React Route Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //Components
 import HeaderNav from "./components/header-nav/header-nav.component";
 import HomePage from "./pages/home-page/home.pages";
-import BarbersPage from "./pages/barbers-page/barbers.pages";
 import ClientHubPage from "./pages/clientHub-page/clientHub.pages";
 //Styles
 import "./App.css";
@@ -16,9 +15,6 @@ const App = () => (
       <Switch>
         <Route exact path="/">
           <HomePage />
-        </Route>
-        <Route path="/barbers">
-          <BarbersPage />
         </Route>
         <Route path="/clientHub">
           <ClientHubPage />
