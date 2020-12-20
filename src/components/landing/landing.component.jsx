3be99b7e509d27c7d1ec0db4d8daf5ec02293e8f
Donte Ravae <donteravae@gmail.com>
2020-12-20 17:23:18 -0500

    added section for welcome and welcome message and sidebar for the appointment queue. Extracted landing background outside of landing section into container section element and added basic styling for welcome section.

--- src/components/landing/landing.component.jsx
+++ src/components/landing/landing.component.jsx
@@ -1,15 +1,23 @@
 //React
-import React from 'react';
+import React from "react";
 //Components
-import AppointmentForm from '../appointment-form/appointment-form.component';
+// import AppointmentForm from "../appointment-form/appointment-form.component";
 //Styles
-import './landing.styles.scss';
+import "./landing.styles.scss";
 
 const Landing = () => (
-    <section className="landing">
-        <div className="landing-background" />
-        <AppointmentForm />
-    </section>
+  <section className="landing-container">
+    <div className="landing-background" />
+    <fragment className="landing">
+      <div className="welcome">
+        <h1 className="welcome-msg">
+          Life Isn't Perfect, But Your Hair Can Be!
+        </h1>
+      </div>
+      {/*    <AppointmentForm /> */}
+    </fragment>
+    <aside className="appointment-queue"></aside>
+  </section>
 );
 
 export default Landing;
