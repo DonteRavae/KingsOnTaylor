2809b8295e54c12b1a35d767557399a14b4e786d
Donte Ravae <donteravae@gmail.com>
2020-12-22 00:21:20 -0500

    created AppointmentScheduler component and styling. Backend linking still needed.

---
+++ src/components/appointment-scheduler/appointment-scheduler.component.jsx
@@ -0,0 +1,39 @@
+//React
+import React from "react";
+//Component
+import CustomButton from "../custom-button/custom-button.component";
+//Styles
+import "./appointment-scheduler.styles.scss";
+
+const AppointmentScheduler = ({ mini }) => {
+  return (
+    <form className="appointment-scheduler">
+      <h2>Life is perfect but your hair can be!</h2>
+      <span>Schedule an appointment today!</span>
+      <section className="barber-and-time-container">
+        <select
+          className="barber-and-time-options"
+          name="barber"
+          id="barberSelection"
+        >
+          <option value="Choose A Barber" className="barber-options">
+            Choose A Barber
+          </option>
+        </select>
+        <select
+          className="barber-and-time-options"
+          name="apptTime"
+          id="apptTimeSelection"
+        >
+          <option value="Choose A Time" className="apptTime-options">
+            Choose An Appointment Time
+          </option>
+        </select>
+      </section>
+      <CustomButton title="Schedule An Appointment Today!" />
+      {/* <button className="submit-appt-btn">Schedule An Appointment</button> */}
+    </form>
+  );
+};
+
+export default AppointmentScheduler;
