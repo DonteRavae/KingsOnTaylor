8a5cef03ad019e91e02aa9b79815e8075b03d7d4
Donte Ravae <41207191+DonteRavae@users.noreply.github.com>
2020-12-22 14:57:50 -0500

    implemented useEffect on HomePage component to fetch the day's appointments and passed appointments into Landing component.

--- src/pages/home-page/home.pages.jsx
+++ src/pages/home-page/home.pages.jsx
@@ -1,22 +1,35 @@
 //React
-import React from "react";
+import React, { useState, useEffect } from "react";
 //Components
 import Landing from "../../components/landing/landing.component";
 // import SectionSeparator from "../../components/section-separator/section-separator.component";
-import FamilyAtmosphere from "../../components/family-atmosphere/family-atmosphere.component";
-import DetailAndPrecision from "../../components/detail-and-precision/detail-and-precision.component";
-import MembersOverview from "../../components/members-overview/members-overview.component";
+// import FamilyAtmosphere from "../../components/family-atmosphere/family-atmosphere.component";
+// import DetailAndPrecision from "../../components/detail-and-precision/detail-and-precision.component";
+// import MembersOverview from "../../components/members-overview/members-overview.component";
 //Styles
 import "./home.styles.scss";
 
 const HomePage = () => {
+  const [appointments, setAppointments] = useState(null);
+  
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      const res = await fetch('http://localhost:3000/appointments/today');
+      return await res.json();
+    }
+    
+    setAppointments(fetchAppointments());
+  })
+  
+  
+  
   return (
     <main className="App">
-      <Landing />
+      <Landing appointments={appointments} />
       {/* <SectionSeparator msg="Heavy Is The Head That Wears The Crown" /> */}
-      <FamilyAtmosphere />
+<!--       <FamilyAtmosphere />
       <DetailAndPrecision />
-      <MembersOverview />
+      <MembersOverview /> -->
     </main>
   );
 };
