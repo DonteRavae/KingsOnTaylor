e3d94676eeb270a27cb2c345e808c95c0ffe6350
Donte Ravae <DonteRavae@gmail.com>
2021-01-09 04:04:21 -0500

    Added conditional to choose between SignInAndSignUp component and ClientPortal component

--- src/pages/clientHub-page/clientHub.pages.jsx
+++ src/pages/clientHub-page/clientHub.pages.jsx
@@ -1,14 +1,23 @@
 //React
 import React from "react";
+import { useSelector } from "react-redux";
+import ClientPortal from "../../components/client-portal/client-portal.component";
 //Components
+// import ClientPortal from "../../components/client-portal/client-portal.component";
 import SignInAndSignUp from "../../components/sign-in-and-sign-up/sign-in-and-sign-up.component";
 //Styles
 import "./clientHub.styles.scss";
 
 const ClientHubPage = () => {
+  const currentUser = useSelector(({ user }) => user.currentUser);
+  console.log(currentUser);
   return (
     <main className="client-hub">
-      <SignInAndSignUp />
+      {currentUser !== null ? (
+        <ClientPortal currentUser={currentUser} />
+      ) : (
+        <SignInAndSignUp />
+      )}
     </main>
   );
 };
