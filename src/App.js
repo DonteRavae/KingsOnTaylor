2ba28ccccb1469c5954e4e76ff52460d5150dcd4
Donte Ravae <DonteRavae@gmail.com>
2021-01-12 02:38:21 -0500

    updated app.css body element tag and refactored routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 //React
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 //Components
 import HeaderNav from "./components/header-nav/header-nav.component";
 import HomePage from "./pages/home-page/home.pages";
-import ClientHubPage from "./pages/clientHub-page/clientHub.pages";
+import ClientPortal from "./components/client-portal/client-portal.component.jsx";
+import SignInAndSignUp from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 //Styles
 import "./App.css";
 
-const App = () => (
-  <Router>
-    <main className="App">
-      <HeaderNav />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/clientHub">
-          <ClientHubPage />
-        </Route>
-      </Switch>
-    </main>
-  </Router>
-);
+const App = () => {
+  const currentUser = useSelector(({ user }) => user.currentUser);
+  return (
+    <Router>
+      <main className="App">
+        <div className="background-img" />
+        <HeaderNav />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route
+            exact
+            path="/clientHub"
+            render={() =>
+              !currentUser ? (
+                <SignInAndSignUp />
+              ) : (
+                <Redirect to={`/clientHub/${currentUser._id}`} />
+              )
+            }
+          />
+          <Route
+            exact
+            path={`/clientHub/${currentUser ? currentUser._id : ""}`}
+            render={() => <ClientPortal currentUser={currentUser} />}
+          />
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
